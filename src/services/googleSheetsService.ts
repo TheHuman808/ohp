f61b953ecf530c5b74697edd052d3e338dc0e580
TheHuman808 <alex.brasaus@gmail.com>
2025-10-20 23:22:05 +0300

    Update googleSheetsService.ts

--- src/services/googleSheetsService.ts
+++ src/services/googleSheetsService.ts
@@ -54,7 +54,7 @@ class GoogleSheetsService {
   constructor() {
     this.spreadsheetId = import.meta.env.VITE_GOOGLE_SHEETS_ID || '1fh4-V4n0ho-RF06xcxl0JYxK5xQf8WOMSYy-tF6vRkU';
     this.apiKey = import.meta.env.VITE_GOOGLE_SHEETS_API_KEY || 'AIzaSyD1-O9ID7-2EFVum1ITNRyrhJYtvlY5wKg';
-    this.webAppUrl = import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbzsMyAyZx5GPFoHTxZzOfE1HnMoaEwIESra0T__yGlYZmQnFDod75lVw4e_M3yLOfId/exec';
+    this.webAppUrl = import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycby3oAvT_HJB9Q0x68jSMeFHbnpq4PCGndYK2uE3FauhJPhz0LFq-vcQYcsL-Bdpr3ww/exec';
     
     console.log('GoogleSheetsService initialized:');
     console.log('Spreadsheet ID:', this.spreadsheetId ? `${this.spreadsheetId.substring(0, 10)}...` : 'NOT SET');
